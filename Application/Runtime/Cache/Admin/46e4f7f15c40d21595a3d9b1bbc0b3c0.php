719f8813005c785e65d7606c4dc1c4f92200803b
Shinecho <echowht@qq.com>
2016-06-17 15:42:33 +0800

    删除缓存文件

--- Application/Runtime/Cache/Admin/46e4f7f15c40d21595a3d9b1bbc0b3c0.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><h1 style="color: #008000;">Green</h1> 
-
-<p><?php echo ($name); ?></p>
-<a href="<?php echo U('index',array('t'=>'default'));?>">Default</a>
-<a href="<?php echo U('index',array('t'=>'green'));?>">Green</a>
-<p><?php echo ((isset($mobile) && ($mobile !== ""))?($mobile):"木有啊"); ?></p>
-<p>项目目录：<?php echo ($_SERVER['DOCUMENT_ROOT']); ?></p>
-
-<nav>这是一个公共的导航栏</nav>
