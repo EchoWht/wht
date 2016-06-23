73dc7c3782eb2d235f65416d46645f09dbef211b
Shinecho <echowht@qq.com>
2016-06-23 15:33:15 +0800

    框架集成微博登录

--- Application/Login/Controller/IndexController.class.php
+++ Application/Login/Controller/IndexController.class.php
@@ -94,7 +94,7 @@ class IndexController extends Controller {
 		if ($token) {
 			$_SESSION['token'] = $token;
 			setcookie('weibojs_' . $o->client_id, http_build_query($token));
-			dump("OK");
+			$this->success('登录成功', '/User/Index/user');
 		} else {
 			dump("Error");
 		}
