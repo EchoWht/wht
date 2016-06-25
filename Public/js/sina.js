d1e81dca4418be61b7cf8b9af472a0be821b1a67
Shinecho <echowht@qq.com>
2016-06-25 10:44:31 +0800

    完善微博登录功能

--- Public/js/sina.js
+++ Public/js/sina.js
@@ -64,3 +64,32 @@ $("#inp-email").blur(
         }); 
     }
 )
+$('#step_one').click(
+    function () {
+        var email=$("#inp-email").val();
+        var username=$("#inp-username").val();
+        var passwd=$("#inp-passwd").val();
+        $.ajax({
+            type:'get',
+            url:host+'/index.php/User/Set/sinabound',
+            data:{
+                username:username,
+                email:email,
+                passwd:passwd
+            },
+            success:function(result){
+                if (result==1001){
+                    $(".alert").html("注册失败请重试").fadeIn()
+                }else if(result==1002){
+                    $(".alert").html("该用户已经绑定").fadeIn()
+                }else if(result==1003){
+                    $(".alert").html("密码错误").fadeIn()
+                }else if(result==1004){
+                    $(".alert").html("绑定失败").fadeIn()
+                }else {
+                    
+                }
+            }
+        });
+    }
+)
