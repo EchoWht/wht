da1165954e405af11805e766c587e66843e4202f
wanghaotian <echowht@qq.com>
2016-07-26 08:48:27 +0800

    更新测试模块

--- Application/Test/Controller/IndexController.class.php
+++ Application/Test/Controller/IndexController.class.php
@@ -1,20 +1,25 @@
 <?php
 namespace Test\Controller;
+
 use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
-        $arr=array(
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $arr = array(
+            'result'=>1,
             array(
-                'id'=>1,
-                'usename'=>'Blskye',
-                'title'=>"Hello World",
-                'content'=>'Hello !!!!!!'
+                'id' => 1,
+                'usename' => 'Blskye',
+                'title' => "Hello World",
+                'content' => 'Hello !!!!!!'
             ),
             array(
-                'id'=>2,
-                'usename'=>'Blskye',
-                'title'=>"Hello World",
-                'content'=>'Hello !!!!!!'
+                'id' => 2,
+                'usename' => 'Blskye',
+                'title' => "Hello World",
+                'content' => 'Hello !!!!!!'
             ),
         );
         $this->ajaxReturn($arr);
